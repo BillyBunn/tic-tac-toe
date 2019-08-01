d42acba7a158a084ff66ba58ef813ca09e2593c4
Billy Bunn <williammbunn@gmail.com>
2019-08-01 14:07:38 -0700

    adds starter for redux, title and links

---
+++ redux/src/components/Board.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component {
+  renderSquare(i) {
+    const winner =
+      this.props.winningSquares && this.props.winningSquares.includes(i);
+    return (
+      <Square
+        key={i}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        winningSquare={winner}
+      />
+    );
+  }
+  render() {
+    let board = [];
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(this.renderSquare(i * 3 + j));
+      }
+      board.push(
+        <div key={i} className="board-row">
+          {row}
+        </div>
+      );
+    }
+
+    return <div>{board}</div>;
+  }
+}
+
+export default Board;
